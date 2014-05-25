9579eb17ff3daffbdd51a84e0c8ed9aae183830d
KimCheolJin <cjfwls1070@naver.com>
2014-05-25 21:03:56 +0900

    Commit - Character&ETC

--- src/com/teamcriminals/Character/Zero.java
+++ src/com/teamcriminals/Character/Zero.java
@@ -1,5 +1,28 @@
 package com.teamcriminals.Character;
 
-public class Zero {
+
+import com.teamcriminals.Entity.Character;
+
+public class Zero extends Character {
+	
+	//생성자
+			public Zero(int h, int mh, int l, int sp, int bo){
+				super.start(h, mh, l, sp, bo); //super의 start를 이용
+			}
+			
+	//제로의 스킬사용 우선 콘솔로구현 나중에 스킬 클래스 가지게되면 그거 사용
+			
+			public void skillZ(){
+				System.out.println("제로의 스킬 Z사용");
+			}
+			
+			public void skillX(){
+				System.out.println("제로의 스킬 X사용");
+			}
+			
+			public void skillC(){
+				System.out.println("제로의 스킬 C사용");
+			}
+
 
 }
